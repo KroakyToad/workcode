06d1c05b5e002e396b207eb2bc872ad5e3281535
Kroaky Toad <31759564+KroakyToad@users.noreply.github.com>
2017-11-03 15:07:27 +0530

    adding roostify

--- ScenarioBuilder/src/main/java/com/infrrd/roostify/testcase/HomePageTestCase.java
+++ ScenarioBuilder/src/main/java/com/infrrd/roostify/testcase/HomePageTestCase.java
@@ -39,28 +39,29 @@ public class HomePageTestCase extends BaseTest {
 	
 	@Test(description = "Validating presence of relevant fields for purchase ", enabled=true)
 	public void validateOptionsForPurchase() throws InterruptedException{
-		HomePagePageLibrary homePagePageLibrary = new HomePagePageLibrary(driver);
+		HomePagePageLibrary homePagePageLibrary = new HomePagePageLibrary(getDriver());
 		response = homePagePageLibrary.addUserInputForPurchase();
 		Assert.assertEquals(response.contains("Success"), true);
 	}
 
 	@Test(description = "Validating presence of relevant fields for refinance ", enabled=true)
 	public void validateOptionsForRefinance() throws InterruptedException{
-		HomePagePageLibrary homePagePageLibrary = new HomePagePageLibrary(driver);
+		HomePagePageLibrary homePagePageLibrary = new HomePagePageLibrary(getDriver());
 		response = homePagePageLibrary.addUserInputForRefinance();
 		Assert.assertEquals(response.contains("Success"), true);
+		
 	}
 
 	@Test(description = "Validating presence of relevant fields for refinance cashout", enabled=true)
 	public void validateOptionsForRefinanceCashout(){
-		HomePagePageLibrary homePagePageLibrary = new HomePagePageLibrary(driver);
+		HomePagePageLibrary homePagePageLibrary = new HomePagePageLibrary(getDriver());
 		response =  homePagePageLibrary.addUserInputForRefinanceCashOut();
 		Assert.assertEquals(response.contains("Success"), true);
 	}
 
 	@Test(description = "Validating graph legends", enabled=true)
 	public void validateGraphLegends(){
-		HomePagePageLibrary homePagePageLibrary = new HomePagePageLibrary(driver);
+		HomePagePageLibrary homePagePageLibrary = new HomePagePageLibrary(getDriver());
 		response = homePagePageLibrary.verifyingGraphLegends();
 		Assert.assertEquals(response.contains("Success"), true);
 	}
@@ -68,35 +69,35 @@ public class HomePageTestCase extends BaseTest {
 
 	@Test(description = "Validating other optimizations section", enabled=true)
 	public void validateOtherOptimizations(){
-		HomePagePageLibrary homePagePageLibrary = new HomePagePageLibrary(driver);
+		HomePagePageLibrary homePagePageLibrary = new HomePagePageLibrary(getDriver());
 		response = homePagePageLibrary.validateOtherOptimizationsAndOptions();
 		Assert.assertEquals(response.contains("Success"), true);
 	}
 
 	@Test(description = "Validating Lead form upon clicking get this loan button ", enabled=true)
 	public void validatingLeadForm(){
-		HomePagePageLibrary homePagePageLibrary = new HomePagePageLibrary(driver);
+		HomePagePageLibrary homePagePageLibrary = new HomePagePageLibrary(getDriver());
 		response = homePagePageLibrary.validatingLeadFormDetailsContainer();
 		Assert.assertEquals(response.contains("Success"), true);
 	}
 
 	@Test(description = "Validating Get My Options is not enabled if madatory data is not filled", enabled=true)
 	public void checkEnablingOfGetMyOptionsButton(){
-		HomePagePageLibrary homePagePageLibrary = new HomePagePageLibrary(driver);
+		HomePagePageLibrary homePagePageLibrary = new HomePagePageLibrary(getDriver());
 		response = homePagePageLibrary.getMyOptionsEnablingCheck();
 		Assert.assertEquals(response.contains("Success"), true);
 	}
 
 	@Test(description = "Validating Zip code error block", enabled=true)
 	public void validateZipCodeError(){
-		HomePagePageLibrary homePagePageLibrary = new HomePagePageLibrary(driver);
+		HomePagePageLibrary homePagePageLibrary = new HomePagePageLibrary(getDriver());
 		response = homePagePageLibrary.validateZipCodeLookupError();
 		Assert.assertEquals(response.contains("Success"), true);
 	}
 
 	@Test(description = "Validate zip code look up container", enabled=true)
 	public void validateZipCodeLookUp(){
-		HomePagePageLibrary homePagePageLibrary = new HomePagePageLibrary(driver);
+		HomePagePageLibrary homePagePageLibrary = new HomePagePageLibrary(getDriver());
 		response =  homePagePageLibrary.validateZipCodeLookUpContainer();
 		Assert.assertEquals(response.contains("Success"), true);
 	}
