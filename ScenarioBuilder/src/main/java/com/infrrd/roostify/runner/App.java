0544f78df8729c9efe2c0af4d71c7f4eb525fd91
Kroaky Toad <31759564+KroakyToad@users.noreply.github.com>
2017-11-03 13:17:52 +0530

    adding scenario builder

---
+++ ScenarioBuilder/src/main/java/com/infrrd/roostify/runner/App.java
@@ -0,0 +1,11 @@
+package com.infrrd.roostify.runner;
+
+import com.kroakyhub.testfrog.runner.TestFrog;
+
+public class App {
+	public static void main(String[] args){
+		TestFrog testFrog = new TestFrog(args);
+		testFrog.run();
+	}
+}
+
